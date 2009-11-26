2f788260eb718d8a9295fa1c5311aa0b6cb6e260
unknown <Hilke@.(none)>
2009-11-26 16:50:57 +0100

    new uncommitted files

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,41 @@
+class Admin::PostsController < ApplicationController
+
+  before_filter :login_required, :except => [:show, :index]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    @post.save!
+    redirect_to @post
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update_attributes!(params[:post])
+    redirect_to @post
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "Deleted post #{@post.title}"
+    redirect_to admin_posts_path
+  end
+
+end
+
