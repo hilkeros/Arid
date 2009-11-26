5c589b06f7dcd4da8a12139bdb9613ffb350cb65
unknown <Hilke@.(none)>
2009-11-26 16:21:30 +0100

    i try my first commit
    changed the views, made difference between admin view and normal view

--- config/routes.rb
+++ config/routes.rb
@@ -25,10 +25,10 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
+  map.namespace :admin do |admin|
+    # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
+       admin.resources :posts
+     end
   
   map.resources :posts, :has_many => :comments
   map.root :controller => "posts"
