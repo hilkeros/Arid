e3d26d1bd9bac9ede7fc23a653f233a5f47ab6d0
Joren De Groof <info@jorendegroof.be>
2009-12-03 17:41:46 +0100

    notice message when registerd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     @user.save!
+    flash[:notice] = "Congratulations, you have succesfully registered an account."
     redirect_to root_path
   rescue ActiveRecord::RecordInvalid
     render :new
