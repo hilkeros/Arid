aaaae403be402f678d160cbf6764eb39fba9fff3
Joren De Groof <info@jorendegroof.be>
2009-12-04 11:13:01 +0100

    Fixed comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   before_filter :login_required
   
   def create
-    @post = Post.find(params[:post_id])
+    @post = Post.find_by_permalink!(params[:post_id])
     @comment = current_user.comments.new(params[:comment])
     @comment.post = @post
     @comment.save
