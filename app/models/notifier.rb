db89820df09c8938f5c907970b51973a44cad144
Joren De Groof <info@jorendegroof.be>
2009-12-04 15:16:22 +0100

    updated from field when sending message

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -2,7 +2,7 @@ class Notifier < ActionMailer::Base
 
   def password_reset_instructions(user)  
     subject       "Arid.be - Password Reset Instructions"  
-    from          "Arid"  
+    from          "Arid <info@arid.be>"  
     recipients    user.email  
     sent_on       Time.now  
     body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token)  
