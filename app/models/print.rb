8f9fab5761d301b5cc15dd3f3d5c139c195924ee
Joren De Groof <info@jorendegroof.be>
2010-01-06 10:08:13 +0100

    installed tinymce

---
+++ app/models/print.rb
@@ -0,0 +1,13 @@
+class Print < ActiveRecord::Base
+  
+  has_attached_file :image,
+    :convert_options => { :quality =>  4 },
+    :styles => { :small_thumb => [ "50x50", :jpg ],
+	  :medium_thumb => [ "100x100", :jpg ],
+    :large_thumb => [ "370x370", :jpg ],
+	  :detail_preview => [ "450x338", :jpg ] },
+    :url  => "/assets/prints/:id/:basename.:extension",
+    :path => ":rails_root/public/assets/prints/:id/:basename.:extension"
+    :default_url => "/images/missing/prints/:style.png"
+
+end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,7 @@
 class Video < ActiveRecord::Base
-
+  
   has_attached_file :original,
                     :url  => "/assets/movies/:id/:basename.:extension",
                     :path => ":rails_root/public/assets/movies/:id/:basename.:extension"
+
 end
