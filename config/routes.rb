c1b947908584fdf74315afa8c5006dd7bec4be2a
Joren De Groof <info@jorendegroof.be>
2010-01-19 14:30:29 +0100

    Update user settings

--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,7 @@ ActionController::Routing::Routes.draw do |map|
   
   map.resource :user_session
   map.resource :account, :controller => "users"
-  map.resources :users
+  map.resource :user
   map.resources :gigs
   map.resources :mailing_addresses
   map.resources :password_resets
