31e81881f4c2a0e488386c3f4b8f967eb0fd12e1
Joren De Groof <info@jorendegroof.be>
2010-02-17 16:32:02 +0100

    added image to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ActiveRecord::Base
+  
+    has_attached_file :image,
+      :styles => { 
+          :thumb => [ "100x100>", :jpg ], 
+          :medium => [ "450x450>", :jpg ], 
+          :large => [ "700x500>", :jpg ] 
+      },
+      :url  => "/assets/products/:style/:id.:extension",
+      :path => ":rails_root/public/assets/products/:style/:id.:extension"
+    
 end
