a86edf1fc034893ed958f0b523a16362c696a96d
Joren De Groof <info@jorendegroof.be>
2010-02-23 15:39:59 +0100

    Use more precise time calculation for futur and past gigs

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -1,23 +1,23 @@
 class Gig < ActiveRecord::Base
 
-    validates_presence_of :date
-    validates_presence_of :venue
-    validates_presence_of :venue_url
-    validates_presence_of :city
-    validates_presence_of :country
+  validates_presence_of :date
+  validates_presence_of :venue
+  validates_presence_of :venue_url
+  validates_presence_of :city
+  validates_presence_of :country
 
-    has_many :comments
-    has_many :photos
-    has_many :videos
-    named_scope :futur, :conditions => ["date > ?", Time.now.midnight]
-    named_scope :past, :conditions => ["date < ?", Time.now.midnight]
+  has_many :comments
+  has_many :photos
+  has_many :videos
+  named_scope :futur, :conditions => ["date > ?", Time.current.beginning_of_day]
+  named_scope :past, :conditions => ["date < ?", Time.current.end_of_day]
 
-    acts_as_url :venue_url
-    
-    has_permalink [:date, :venue]
-    
-    def to_param
-      permalink
-    end
+  acts_as_url :venue_url
 
+  has_permalink [:date, :venue]
+
+  def to_param
+    permalink
+  end
+  
 end
