6d3df72926d77f2761de66d77775694ce5eccc2c
Joren De Groof <info@jorendegroof.be>
2010-02-24 11:20:38 +0100

    Added video to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
-  # belongs_to :photo
+  belongs_to :photo
   # belongs_to :video
   
   def target
-    post
+    post || photo
   end
 end
