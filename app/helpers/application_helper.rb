3ca2dd51275cb09707039408290f0b480b20ffec
Joren De Groof <info@jorendegroof.be>
2010-02-24 11:08:39 +0100

    fixed activity following link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
     when Comment
       "#{activity.user.name} made a comment on the blog post #{link_to activity.target.title post_path(activity.target)}"
     when Friendship
-      "#{activity.user.name} is now following #{link_to activity.target.name, user_path(activity.target)}"
+      "#{activity.user.name} is now following #{link_to activity.target.name, profile_path(activity.target)}"
     when Photo
       "#{activity.user.name} has uploaded a #{link_to 'photo', user_photo_path(activity.user, activity.action)}"
     else
