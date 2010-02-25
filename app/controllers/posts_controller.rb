8dd2a01ee57ba8229b0ad594a8c53ca8cccb56fb
Joren De Groof <info@jorendegroof.be>
2010-02-25 09:56:34 +0100

    Paginate news

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController  
   def index
-    @posts = Post.public.all(:order => "created_at DESC")
+    @posts = Post.public.paginate(:all, :per_page => 10, :page => params[:page], :order => "created_at DESC")
     respond_to do |format|
       format.html
       format.atom
