d5a135fa2f5130713c3d8be735c71f6efe8d0b8f
Joren De Groof <info@jorendegroof.be>
2010-02-25 15:17:34 +0100

    basic videos

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,5 +3,7 @@ class Video < ActiveRecord::Base
   has_attached_file :original,
                     :url  => "/assets/media/:id/:basename.:extension",
                     :path => ":rails_root/public/assets/media/:id/:basename.:extension"
-
+                    
+  named_scope :uploaded, :conditions => "url IS NULL"
+  named_scope :links, :conditions => "url IS NOT NULL"
 end
