b7a000698df8cc30139a57879ae0eafbd9f21948
Joren De Groof <info@jorendegroof.be>
2010-02-25 14:44:54 +0100

    log shouts and translated activity log

--- app/models/activity_logger.rb
+++ app/models/activity_logger.rb
@@ -1,5 +1,5 @@
 class ActivityLogger < ActiveRecord::Observer
-  observe :comment, :friendship, :photo
+  observe :comment, :friendship, :photo, :shout
   
   def after_create model
     Activity.create(:user_id => model.user_id, :target => model.target, :action => model)
