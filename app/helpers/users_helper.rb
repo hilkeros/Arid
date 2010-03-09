59c3cc5cdbc7e7250ea2fa6b507c88ae8467aa4d
Joren De Groof <info@jorendegroof.be>
2010-03-09 09:37:47 +0100

    Fix social network links if users are stupid

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,35 @@
 module UsersHelper
+  
+  def myspace_link uid
+    case
+    when uid.match(/^^(www.)?myspace.com\/\w+$/)
+      link_to(image_tag('icons/social/IconMyspaceSml.png'), "http://#{uid}")
+    when !uid.match(/myspace.com/)
+      link_to(image_tag('icons/social/IconMyspaceSml.png'), "http://myspace.com/#{uid}")
+    else
+      link_to(image_tag('icons/social/IconMyspaceSml.png'), uid)
+    end
+  end
+  
+  def twitter_link uid
+      case
+      when uid.match(/^^(www.)?twitter.com\/\w+$/)
+        link_to(image_tag('icons/social/IconTwitterSml.png'), "http://#{uid}")
+      when !uid.match(/twitter.com/)
+        link_to(image_tag('icons/social/IconTwitterSml.png'), "http://twitter.com/#{uid}")
+      else
+        link_to(image_tag('icons/social/IconTwitterSml.png'), uid)
+      end
+    end
+    
+    def netlog_link uid
+      case
+      when uid.match(/^^(www.)?netlog.com\/\w+$/)
+        link_to(image_tag('icons/social/IconNetlogSml.png'), "http://#{uid}")
+      when !uid.match(/netlog.com/)
+        link_to(image_tag('icons/social/IconNetlogSml.png'), "http://netlog.com/#{uid}")
+      else
+        link_to(image_tag('icons/social/IconNetlogSml.png'), uid)
+      end
+    end
 end
